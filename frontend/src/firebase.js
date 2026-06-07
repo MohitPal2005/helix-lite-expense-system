b52dcdf8a7698dc59bd1b069e39943f75ff822e7
MohitPal2005 <mohit799961@gmail.com>
2026-06-07 18:00:46 +0530

    chore: secure Firebase keys in env variables

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAiPMep7muip_FlHOGieqWUymWSOJd_R6M",
-  authDomain: "helix-lite.firebaseapp.com",
-  projectId: "helix-lite",
-  storageBucket: "helix-lite.firebasestorage.app",
-  messagingSenderId: "911531866932",
-  appId: "1:911531866932:web:ba933c2cb568598f974361"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
